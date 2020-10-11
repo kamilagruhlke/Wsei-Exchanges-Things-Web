b89758773b7e55b3eb484b7d1bdf1899fd8a4b41
kamilagruhlke <kgruhlke@gmail.com>
2020-10-11 21:37:11 +0200

    New project

---
+++ Wsei.Exchanges.Things.Web/Controllers/ExchangesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wsei.Exchanges.Things.Web.Controllers
+{
+    public class ExchangesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Wsei.Exchanges.Things.Web/Models/ExchangesModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wsei.Exchanges.Things.Web.Models
+{
+    public class ExchangesModel
+    {
+    }
+}
